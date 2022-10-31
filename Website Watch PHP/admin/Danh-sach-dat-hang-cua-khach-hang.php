b895ef5525b850260042eebe4f881b9072d1a451
nguyenquocchauit <chauquocnguyen.cun1@gmail.com>
2022-10-31 23:51:02 +0700

    update

--- Website Watch PHP/admin/Danh-sach-dat-hang-cua-khach-hang.php
+++ Website Watch PHP/admin/Danh-sach-dat-hang-cua-khach-hang.php
@@ -1,5 +1,6 @@
 <?php
 require '../config/connectDB.php';
+include 'inlcudes_function/list_order.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,61 +28,7 @@ require '../config/connectDB.php';
     ?>
     <div class="body-list-order-detail-customer">
         <div class="container-fluid pt-4">
-            <h4 class=" mb-1 pt-1">Danh sách khách hàng oder</h4>
-            <table>
-                <tr>
-                    <td colspan="6"></td>
-                    <td><button type="button" class="btn btn-secondary">Tải file Excel</button></td>
-                </tr>
-                <tr class="tr1">
-                    <td>STT</td>
-                    <td>
-                        <p>ID_Oder</p>
-                    </td>
-                    <td>
-                        <p>ID_Customer</p>
-                    </td>
-                    <td>
-                        <p>Create_At</p>
-                    </td>
-                    <td>
-                        <p>Status</p>
-                    </td>
-                    <td>
-                        <p>Description</p>
-                    </td>
-                    <td>
-                        <p>Total</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Oder0000122222222222222</td>
-                    <td>KH999999999999999999</td>
-                    <td>25/12/2022</td>
-                    <td class="text-danger">Chưa thanh toán</td>
-                    <td>11111111111111111111111</td>
-                    <td>10.000.000đ</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Oder00001</td>
-                    <td>KH999999999999999999</td>
-                    <td>25/12/2022</td>
-                    <td class="text-success">Đã thanh toán</td>
-                    <td>11111111111111111111111</td>
-                    <td>10.000.000đ</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Oder00001</td>
-                    <td>KH999999999999999999</td>
-                    <td>25/12/2022</td>
-                    <td class="text-danger">Chưa thanh toán</td>
-                    <td>11111111111111111111111</td>
-                    <td>10.000.000đ</td>
-                </tr>
-            </table>
+            <?php Show_List_Orrder($conn); ?>
         </div>
     </div>
     <?php
